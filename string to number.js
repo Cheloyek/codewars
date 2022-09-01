e3ec71bda48f346f6d7267219880715fe9691c91
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-01 23:00:47 +0300

    Update string to number.js

--- string to number.js
+++ string to number.js
@@ -6,12 +6,12 @@ function unusualFive() {
 
 //variant 2
 function unusualFive() {
-  return 'abcde'.length // выведет 5
+  return 'abcde'.length; // выведет 5
 }
 
 // variant 3
 function unusualFive() {
-  let massiv = ["5", "5", "5", "5", "5"]
-  return massiv.length // выведет 5
+  let massiv = ["5", "5", "5", "5", "5"];
+  return massiv.length; // выведет 5
 }
 
