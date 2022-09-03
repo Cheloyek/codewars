d3785f44bbd33335e40f2821750aa9ed2f521325
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-03 23:22:15 +0300

    Update bool true or false.js

--- bool true or false.js
+++ bool true or false.js
@@ -1,8 +1,14 @@
 //функция проверяющая введенную строку
+//variant 1
 function boolToWord( bool ){
   if (bool === true) {
     return "Yes"
   } else {
     return "No"
   }
+}
+
+//variant 2
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
 }
