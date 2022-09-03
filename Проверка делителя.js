8fe0fdf1a510c017f939dac5684ff484cd0e9426
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-04 00:07:56 +0300

    Create Проверка делителя.js

---
+++ Проверка делителя.js
@@ -0,0 +1,8 @@
+//функция проверяет делится ли base на factor без остатка
+function checkForFactor (base, factor) {
+  if (base % factor == 0) {
+    return true
+  } else {
+    return false
+  }
+}
