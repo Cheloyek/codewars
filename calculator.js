28cbd3dc389b07e3899a46a5cfe7acf2dfd02cb6
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-05 15:10:14 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+//variant 1
 function basicOp(operation, value1, value2) { 
   if (operation == '+') {
     return value1 + value2
@@ -8,4 +9,16 @@ function basicOp(operation, value1, value2) {
   } else if (operation == '/') {
     return value1 / value2
   } 
+}
+
+
+//variant 2
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value1 / value2;
+        default: return 'Operation must be one of + - * /';
+    }
 }
