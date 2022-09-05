37c3757fc1130d069a5e400c606a4394f8e3872b
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-05 21:16:07 +0300

    Update Подсчет одинаковых значений в массиве.js

--- Подсчет одинаковых значений в массиве.js
+++ Подсчет одинаковых значений в массиве.js
@@ -1,4 +1,16 @@
+//varuant 1
 function countSheeps(arrayOfSheep) {
   let trueSheep = arrayOfSheep.filter(item => item === true).length //filter - формирует новый массив при помощи функции
   return trueSheep
+}
+
+//variant 2
+function countSheeps(arrayOfSheep) {
+  let num = 0;
+  
+  for(let i = 0; i < arrayOfSheep.length; i++)
+    if(arrayOfSheep[i] == true)
+      num++;
+      
+  return num;
 }
