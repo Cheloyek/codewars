3b133674ea640242bd48e12e1020cd8da110d450
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-05 15:51:26 +0300

    Update зеркальное отражение текста.js

--- зеркальное отражение текста.js
+++ зеркальное отражение текста.js
@@ -1,6 +1,12 @@
+//variant 1
 function solution(str){
   let s = str.split('') //принимает строку и преобразует в массив, разбивая ее по разделителю
   let t = s.reverse() //переворачивает элементы массива
   let r = t.join('') //соединяет все элементы массива в строку
   return r
+}
+
+//variant 2
+function solution(str){
+  return str.split('').reverse().join('');  
 }
