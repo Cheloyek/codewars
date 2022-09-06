29422f7293032a5539a28afa24e0bba479ec01b9
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-06 12:39:07 +0300

    Update Соединение элементов массива.js

--- Соединение элементов массива.js
+++ Соединение элементов массива.js
@@ -2,7 +2,7 @@ function squareDigits(num){
   let stringNum = String(num) //преобразование числа в строку
   let arrayNum = Array.from(stringNum) //преобразование строки в массив
   const newArray = arrayNum.map(el => el **2) //возведение в квадрат каждого значения массива
-  let joinNum = newArray.join('') //соединение элементов массива 
-  console.log (joinNum)
+  let joinNum = newArray.join('') //соединение элементов массива в string
+  console.log (Number(joinNum)) //преобразует string в number и выведет в консоль
 }
 squareDigits (3212)
